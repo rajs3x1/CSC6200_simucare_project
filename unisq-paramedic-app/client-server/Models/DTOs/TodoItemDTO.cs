dc9c246d1a2b962ad518326d85d1241f2b125baa
Raj Subham <rs@rs.com>
2024-06-21 05:29:00 -0700

    Simplified Deployment

---
+++ unisq-paramedic-app/client-server/Models/DTOs/TodoItemDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace client_server.Models.DTOs;
+
+public class TodoItemDTO
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
